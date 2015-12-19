e4b0121c669f0ac39a512135dc5944b94eeec9eb
JHOAN SEBASTIAN ARIAS PULIDO <Sebastian@MacBook-Pro-de-Sebastian.local>
2015-12-19 09:22:40 -0500

    primera version

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(main);
+var connav=0;
+function main(){
+	$('.item a').click(anclas);
+	$('.glyphicon-menu-hamburger').click(menu);
+}
+function anclas(e){
+	e.preventDefault();
+    var href = $(this).attr('href');
+    var altonav=$('nav').innerHeight();
+    console.log(altonav);
+    $('html, body').animate({ scrollTop : $( href ).offset().top-altonav}, 'easeInOutExpo');
+}
+function menu(){
+	if(connav==0){
+		$('nav').animate({
+		left: 0,
+		},500,'easeInQuint',function() {
+    		$('.item a').css({
+    			left: '0',
+    			
+    		});
+  		});
+		connav=1
+	}
+	else{
+		$('nav').animate({
+		left: -300,
+		},500,'easeOutQuint',function() {
+    		$('.item a').css({
+    			left: '-100%',
+    		});
+  		});
+		connav=0
+	}
+	
+}
